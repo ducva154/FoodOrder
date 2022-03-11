21947db795eca3937d8398d139155810a654d439
duynguyennk <nguyenduy.dnk@gmail.com>
2022-03-11 15:55:43 +0700

    add main activity UI and fragment

--- app/src/main/java/fu/prm392/sampl/is1420_project/MainActivity.java
+++ app/src/main/java/fu/prm392/sampl/is1420_project/MainActivity.java
@@ -1,14 +1,48 @@
 package fu.prm392.sampl.is1420_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import fu.prm392.sampl.is1420_project.fragments.UserHomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch(item.getItemId()){
+                    //check id
+                    case R.id.pageHome:
+                        selectedFragment = new UserHomeFragment();
+                        break;
+                    default:
+                        return false;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+                return true;
+            }
+        });
+
+        //default fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new UserHomeFragment()).commit();
+
     }
 }
